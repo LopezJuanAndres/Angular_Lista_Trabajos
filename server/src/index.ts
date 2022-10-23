07ee57e827b92346c381428c2f38ade93a336618
LopezJuanAndres <andres.juan.lopez@gmail.com>
2022-10-23 01:32:18 -0300

    Commit inicial

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,22 @@
-console.log("test de consola 3");
+import express, {Application} from "express";
+class Server {
+    public app:Application;
+    constructor(){
+        this.app=express();
+        this.config();
+        this.routes();
+    }
+    config():void{
+this.app.set('port',process.env.PORT ||3000)
+    }
+    routes():void{
+
+    }
+    start():void {
+        this.app.listen(this.app.get('port'), ()=>{
+            console.log('server on port', this.app.get('port'))
+        } )
+    }
+}
+ const server= new Server();
+ server.start();
